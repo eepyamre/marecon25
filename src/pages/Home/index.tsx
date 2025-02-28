397ce35a72b2f0d33c4cf8ef815ee0353ca957b0
Eepyamre <eepyamre@gmail.com>
2025-02-28 19:18:42 +0300

    upd home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,22 +6,22 @@ import { useSchedule } from '@/helpers/useSchedue';
 import { getPreview } from '@/helpers/getPreview';
 
 const date = 1740771000;
-const day = (() => {
-  const d1 = 1740747600000;
-  const d2 = 1740747600000;
-  if (Date.now() > d2) {
-    return 'sunday';
-  }
-  if (Date.now() > d1) {
-    return 'saturday';
-  }
-  return 'friday';
-})();
 
 export function Home() {
+  const day = (() => {
+    const d1 = 1740841200000;
+    const d2 = 1740927600000;
+
+    if (Date.now() > d2) {
+      return 'sunday';
+    }
+    if (Date.now() > d1) {
+      return 'saturday';
+    }
+    return 'friday';
+  })();
   const { events } = useSchedule(day);
   const video = useRef<HTMLAnchorElement>();
-
   const ch1 = useMemo(() => {
     const now = Date.now();
     const activeItem = events.track1.find((item) => {
@@ -36,6 +36,7 @@ export function Home() {
 
   const ch2 = useMemo(() => {
     const now = Date.now();
+
     const activeItem = events.track2.find((item) => {
       if (!item) return false;
       const endTime = new Date(
@@ -93,66 +94,62 @@ export function Home() {
     };
   }, []);
 
-  if (Date.now() > date * 1000) {
-    return (
-      <div class={css.wrapper}>
-        <div className={css.bg}></div>
-        <h1 class={css.title}>LIV LIV LIV</h1>
-
-        <div class={css.watchOnline}>
-          <div class={css.secondTitle}>
-            <p>
-              Now on Comfy's Cottage:{' '}
-              <span>{ch2?.title || 'Intermission'}</span>
-            </p>
-          </div>
-          <div class={css.btnWrapper}>
-            <a
-              target={'_blank'}
-              href={'https://cytu.be/r/marecon2-comfys-cottage'}
-              class={css.btn}
-            >
-              Watch Online
-            </a>
-          </div>
-        </div>
-
-        <a
-          target={'_blank'}
-          href={'https://cytu.be/r/marecon'}
-          ref={video}
-          class={css.video}
-        >
-          <div className={css.videoTitle}>
-            On Mare's stage <br /> "{ch1?.title || 'Intermission'}"
-          </div>
-          <div class={css.playWrapper}>
-            <svg
-              version='1.1'
-              xmlns='http://www.w3.org/2000/svg'
-              width='979'
-              height='1024'
-              viewBox='0 0 979 1024'
-              class={css.play}
-            >
-              <g id='icomoon-ignore'></g>
-              <path
-                d='M918.261 517.846c0.027 10.391-2.64 20.614-7.733 29.674-5.098 9.056-12.448 16.642-21.344 22.016l-551.264 337.23c-9.294 5.694-19.939 8.797-30.835 9.002-10.896 0.2-21.649-2.511-31.147-7.854-9.408-5.258-17.245-12.929-22.705-22.225-5.461-9.292-8.347-19.87-8.363-30.649v-674.388c0.016-10.779 2.902-21.359 8.363-30.652s13.297-16.965 22.705-22.225c9.499-5.343 20.251-8.054 31.147-7.852s21.541 3.308 30.835 9l551.264 337.233c8.895 5.374 16.246 12.96 21.344 22.016 5.093 9.060 7.76 19.282 7.733 29.674z'
-                fill={'currentColor'}
-              ></path>
-            </svg>
-            <p class={css.cytube}>Watch on Cytube</p>
-          </div>
-        </a>
-      </div>
-    );
-  }
-
   return (
     <div class={css.wrapper}>
-      <h1 class={css.title}>(\ Soon /)</h1>
-      <h2 class={css.timer}>{remainig}</h2>
-      <img class={css.img} src={teaParty} alt='mmm yummy tea ' />
+      <div className={css.bg}></div>
+      {Date.now() < date * 1000 ? (
+        <>
+          <h1 class={css.title}>Starting soon!</h1>
+          <h2 class={css.timer}>{remainig}</h2>
+        </>
+      ) : (
+        <h1 class={css.title}>We're live!</h1>
+      )}
+
+      <div class={css.watchOnline}>
+        <div class={css.secondTitle}>
+          <p>
+            Now on Comfy's Cottage: <span>{ch2?.title || 'Intermission'}</span>
+          </p>
+        </div>
+        <div class={css.btnWrapper}>
+          <a
+            target={'_blank'}
+            href={'https://cytu.be/r/marecon2-comfys-cottage'}
+            class={css.btn}
+          >
+            Watch Online
+          </a>
+        </div>
+      </div>
+
+      <a
+        target={'_blank'}
+        href={'https://cytu.be/r/marecon'}
+        ref={video}
+        class={css.video}
+      >
+        <div className={css.videoTitle}>
+          On The Main Stage <br /> {ch1?.title || 'Intermission'}
+        </div>
+        <div class={css.playWrapper}>
+          <svg
+            version='1.1'
+            xmlns='http://www.w3.org/2000/svg'
+            width='979'
+            height='1024'
+            viewBox='0 0 979 1024'
+            class={css.play}
+          >
+            <g id='icomoon-ignore'></g>
+            <path
+              d='M918.261 517.846c0.027 10.391-2.64 20.614-7.733 29.674-5.098 9.056-12.448 16.642-21.344 22.016l-551.264 337.23c-9.294 5.694-19.939 8.797-30.835 9.002-10.896 0.2-21.649-2.511-31.147-7.854-9.408-5.258-17.245-12.929-22.705-22.225-5.461-9.292-8.347-19.87-8.363-30.649v-674.388c0.016-10.779 2.902-21.359 8.363-30.652s13.297-16.965 22.705-22.225c9.499-5.343 20.251-8.054 31.147-7.852s21.541 3.308 30.835 9l551.264 337.233c8.895 5.374 16.246 12.96 21.344 22.016 5.093 9.060 7.76 19.282 7.733 29.674z'
+              fill={'currentColor'}
+            ></path>
+          </svg>
+          <p class={css.cytube}>Watch on Cytube</p>
+        </div>
+      </a>
     </div>
   );
 }

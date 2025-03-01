a47f8ff7641cc5c47e2b71fddf726295509992bd
Eepyamre <eepyamre@gmail.com>
2025-03-02 00:12:35 +0300

    merch preview

--- src/components/Shop/index.tsx
+++ src/components/Shop/index.tsx
@@ -3,6 +3,10 @@ import numget from '@/assets/images/numbeam.png';
 import nawni from '@/assets/images/nawni_shop.png';
 import play from '@/assets/images/play.png';
 import colgate from '@/assets/images/jam/colgate.png';
+import merch1 from '@/assets/images/merch/0.webp';
+import merch2 from '@/assets/images/merch/1.webp';
+import merch3 from '@/assets/images/merch/2.webp';
+import merch4 from '@/assets/images/merch/3.webp';
 import css from './styles.module.scss';
 
 export const Shop = () => {
@@ -12,35 +16,36 @@ export const Shop = () => {
   return (
     <div class={css.wrapper}>
       <div class={css.bg}></div>
-      {/* <div className={css.content}>
+      <div className={`${css.content} ${css.contentMerch}`}>
         <div class={css.header}>
           <img src={nawni} class={css.nawni} alt='shopkeeper' />
           <h2>Marecon Merch </h2>
         </div>
         <div class={css.items}>
-          <a target={'_blank'} href={'#'} class={css.item}>
-            <img src={numget} alt='image' />
-            <p class={css.itemTitle}>My little Numget </p>
+          <div className={css.placeholder}>coming soon!</div>
+          <a target={'_blank'} class={css.item}>
+            <img src={merch1} alt='image' />
+            <p class={css.itemTitle}>T-shirt!? </p>
           </a>
-          <a target={'_blank'} href={'#'} class={css.item}>
-            <img src={numget} alt='image' />
-            <p class={css.itemTitle}>My little Numget </p>
+          <a target={'_blank'} class={css.item}>
+            <img src={merch2} alt='image' />
+            <p class={css.itemTitle}>PIN?</p>
           </a>
-          <a target={'_blank'} href={'#'} class={css.item}>
-            <img src={numget} alt='image' />
-            <p class={css.itemTitle}>My little Numget </p>
+          <a target={'_blank'} class={css.item}>
+            <img src={merch3} alt='image' />
+            <p class={css.itemTitle}>Lanyard!</p>
           </a>
-          <a target={'_blank'} href={'#'} class={css.item}>
-            <img src={numget} alt='image' />
-            <p class={css.itemTitle}>My little Numget </p>
+          <a target={'_blank'} class={css.item}>
+            <img src={merch4} alt='image' />
+            <p class={css.itemTitle}>A PONY (REAL)</p>
           </a>
         </div>
-        <div>
+        {/* <div>
           <a href='#' class={css.btn}>
             SEE ALL PRODUCTS
           </a>
-        </div>
-      </div> */}
+        </div> */}
+      </div>
 
       <div className={css.content}>
         <div class={`${css.header} ${css.header2}`}>

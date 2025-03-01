a746fc5bfe913f41f5ce0953d62958c9fe4382ad
Eepyamre <eepyamre@gmail.com>
2025-03-01 22:03:53 +0300

    play

--- src/components/Shop/index.tsx
+++ src/components/Shop/index.tsx
@@ -2,17 +2,17 @@ import { useLocation } from 'preact-iso';
 import numget from '@/assets/images/numbeam.png';
 import nawni from '@/assets/images/nawni_shop.png';
 import play from '@/assets/images/play.png';
+import colgate from '@/assets/images/jam/colgate.png';
 import css from './styles.module.scss';
 
 export const Shop = () => {
-  return null;
   const location = useLocation();
   if (location.path !== '/') return null;
 
   return (
     <div class={css.wrapper}>
       <div class={css.bg}></div>
-      <div className={css.content}>
+      {/* <div className={css.content}>
         <div class={css.header}>
           <img src={nawni} class={css.nawni} alt='shopkeeper' />
           <h2>Marecon Merch </h2>
@@ -40,7 +40,7 @@ export const Shop = () => {
             SEE ALL PRODUCTS
           </a>
         </div>
-      </div>
+      </div> */}
 
       <div className={css.content}>
         <div class={`${css.header} ${css.header2}`}>
@@ -73,6 +73,40 @@ export const Shop = () => {
               alt='image'
             />
             <p class={css.itemTitle}>Farmare </p>
+          </a>
+          <a
+            target={'_blank'}
+            href={'https://littleinchybit.itch.io/cheharda'}
+            class={css.item}
+          >
+            <img
+              src={
+                'https://img.itch.zone/aW1nLzIwMDgzMjE3LmpwZw==/315x250%23c/OGtLm3.jpg'
+              }
+              alt='image'
+            />
+            <p class={css.itemTitle}>Cheharda</p>
+          </a>
+          <a
+            target={'_blank'}
+            href={'https://konstantin-kasket.itch.io/anon-vs'}
+            class={css.item}
+          >
+            <img
+              src={
+                'https://img.itch.zone/aW1nLzIwMDkyMjYwLnBuZw==/original/yTR6Te.png'
+              }
+              alt='image'
+            />
+            <p class={css.itemTitle}>Anon VS</p>
+          </a>
+          <a
+            target={'_blank'}
+            href={'https://a393.itch.io/colgate-3d-maze'}
+            class={css.item}
+          >
+            <img src={colgate} alt='image' />
+            <p class={css.itemTitle}>Colgate 3d</p>
           </a>
         </div>
       </div>
